0364fdfc1742dfd0c89bef022e64249a64e472fa
Nick Lane <nickyjl22@gmail.com>
2023-05-20 16:10:09 +0100

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Welcome from "./components/Welcome";
+
+export default function App(): JSX.Element {
+  return (
+    <>
+      <div>
+        <Welcome />
+      </div>
+    </>
+  );
+}
---
+++ src/components/Welcome.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Welcome(): JSX.Element {
+  return (
+    <div className="wrapper">
+      <h1>Welcome to my App</h1>
+      <p>This is going to be good</p>
+    </div>
+  );
+}
